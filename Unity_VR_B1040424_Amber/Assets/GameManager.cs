0ae05235550964c1eb70c7d0f4baecdce78db699
arbing89425 <55218462+arbing89425@users.noreply.github.com>
2020-01-06 08:51:52 +0800

    設定基本VR場景

---
+++ Unity_VR_B1040424_Amber/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// 須公開 且參數為零或一
+	public void ThrowPickUp()
+	{
+		
+	}
+	public void ThrowPickDetach(GameObject obj)
+	{
+		Destroy(obj);
+	}
+	public void ThrowPickHeld()
+	{
+		
+	}
+}
